224f92aa240619053951e167837632a60495a1ee
golovachevavp <golovacheva.vp@gmail.com>
2022-09-14 21:48:54 +0300

    2

--- src/number2String.java
+++ src/number2String.java
@@ -56,7 +56,7 @@ public class number2String {
 //        System.out.println(getProverka("foo(bar[i)").equals("10"));
 //        System.out.println(getProverka("()[}").equals("4"));
        System.out.println(getProverka("()").equals("Success"));
-        System.out.println(getProverka("())").equals("3"));
+       System.out.println(getProverka("())").equals("3"));
 //        System.out.println(getProverka("(()").equals("1"));
 //        System.out.println(getProverka("()[{}").equals("3"));
 //        System.out.println(getProverka("(){[}").equals("5"));
@@ -64,7 +64,7 @@ public class number2String {
         System.out.println(getProverka("(())"));
         System.out.println(getProverka("()()"));
         System.out.println(getProverka("()())))"));
-        System.out.println(getProverka("())()"));
+        System.out.println(getProverka("()(()"));
 
     }
 }
