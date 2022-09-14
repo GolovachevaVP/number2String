5692b84b2494430917f936b8ca88a63c34076380
golovachevavp <golovacheva.vp@gmail.com>
2022-09-14 22:40:00 +0300

    4

--- src/number2String.java
+++ src/number2String.java
@@ -1,50 +1,37 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class number2String {
     public static String getProverka(String s) {
         System.out.print("Строка " + s + " - ");
-        int min = s.length();
-        int a = 0, b = 0;
+        List<String> openList = new ArrayList<>();
         String[] str = s.split("");
-        for (int i = 0; i < str.length;i++) {
-            switch (str[i]) {
-                case (")"):
-                    for (int j = 0; j < str.length; j++) {
-                        if (str[j].equals("(") && j < i) {
-                            a++;
-                            continue;
-                        }
-                        if (str[j].equals(")") ) {
-                            b++;
-                            continue;
-                        }
-
-                        if (i != j && min > i) min = i;
-                        break;
-                    }
-                    if (a == b) {
-                        min = s.length();
-                        break;
-                    } else if (min > i) {
-                        min = i+1;
-                        break;
-                    }
-                    break;
-                case ("("):
-
-
-
-
+        for (String element : str) {
+            if (isOpen(element)) openList.add(element);
+            if (isClosed(element)) {
+                String closedBracket = openList.get(openList.size());
+                getOpenBracket(element).equals(closedBracket);
             }
-
         }
-            if (min < s.length()) return "" + (min + 1);
-            else return "Success";
-
-
 
     }
+
+    public static boolean isOpen(String element) {
+        return element.equals("(") || element.equals("[") || element.equals("{");
+    }
+
+    public static boolean isClosed(String element) {
+        return element.equals(")") || element.equals("]") || element.equals("}");
+    }
+
+    public static String getOpenBracket(String element) {
+        if (element.equals(")")) return "(";
+        if (element.equals("]")) return "[";
+        if (element.equals("}")) return "{";
+        return "";
+    }
+
 
     public static void main(String[] args) {
 //        System.out.println(getProverka("[]").equals("Success"));
@@ -55,11 +42,11 @@ public class number2String {
 //        System.out.println(getProverka("foo(bar)").equals("Success"));
 //        System.out.println(getProverka("foo(bar[i)").equals("10"));
 //        System.out.println(getProverka("()[}").equals("4"));
-       System.out.println(getProverka("()").equals("Success"));
-       System.out.println(getProverka("())").equals("3"));
+        System.out.println(getProverka("()").equals("Success"));
+        System.out.println(getProverka("())").equals("3"));
 //        System.out.println(getProverka("(()").equals("1"));
 //        System.out.println(getProverka("()[{}").equals("3"));
-//        System.out.println(getProverka("(){[}").equals("5"));
+        System.out.println(getProverka("(){[}").equals("5"));
 
         System.out.println(getProverka("(())"));
         System.out.println(getProverka("()()"));
